8016987f81ab0e7cfb0df81a9a0650684861bbe4
xicool <frozen_moon@hotmail.co.jp>
2015-01-24 07:37:04 +0900

    GameManaget Commit

---
+++ GGj/Assets/takayama/GameManager/Scripts/GameTimer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour
+{
+
+	public float GameTime = 3 * 60; // 3 minutes;
+
+	public bool GameStarted {
+		get { return mGameStarted; }
+	}
+
+	private bool mGameStarted = false;
+
+	public void GameStart ()
+	{
+		StartCoroutine(CountTimeRoutine());
+		mGameStarted = true;
+	}
+
+	public IEnumerator CountTimeRoutine ()
+	{
+		while(GameTime > 0)
+		{
+			yield return null;
+			GameTime -= Time.deltaTime;
+		}
+		GameTime = 0f;
+		Debug.Log("GameTime Over");
+	}
+
+}
