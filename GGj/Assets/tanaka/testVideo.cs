9da40747acdbacf9d9deb88c5ef23f87e929ddad
田中 主夫 <kwin786@kt-macbook-air.local>
2015-01-23 23:19:46 +0900

    add web template

---
+++ GGj/Assets/tanaka/testVideo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testVideo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("test");
+		Application.ExternalCall("alertUnityObject", " L9 testVido.cs");
+		Application.ExternalCall("showVideo", "R7ttn0eVhQg");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
