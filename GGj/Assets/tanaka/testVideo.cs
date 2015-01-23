bb85f9b737a9c6e986dcfef4d3ba8641295ff6e7
田中 主夫 <kwin786@kt-macbook-air.local>
2015-01-24 02:49:16 +0900

    update index.html

--- GGj/Assets/tanaka/testVideo.cs
+++ GGj/Assets/tanaka/testVideo.cs
@@ -7,7 +7,7 @@ public class testVideo : MonoBehaviour {
 	void Start () {
 		Debug.Log("test");
 		Application.ExternalCall("alertUnityObject", " L9 testVido.cs");
-		Application.ExternalCall("showVideo", "R7ttn0eVhQg,30,40,200,400");
+		Application.ExternalCall("showVideo", "R7ttn0eVhQg,30,40,560,315,200,400,0");
 	}
 	
 	// Update is called once per frame
@@ -21,11 +21,14 @@ public class testVideo : MonoBehaviour {
 			Application.ExternalEval("killVideo()");
 		}
 		if( Input.GetKeyDown(KeyCode.Z) ){
-			Application.ExternalCall("showVideo", "R7ttn0eVhQg,10,15,200,400");
+			Application.ExternalCall("showVideo", "R7ttn0eVhQg,10,15,560,315,200,400,1");
 		}
 		if( Input.GetKeyDown(KeyCode.A) ){
-			Application.ExternalCall("showVideo", "R7ttn0eVhQg,0,0,200,400");
+			Application.ExternalCall("showVideo", "R7ttn0eVhQg,0,0,800,400,200,400,1");
 		}
 
+		if( Input.GetKeyDown(KeyCode.S) ){
+			Application.ExternalCall("showVideo", "R7ttn0eVhQg,0,0,1280,720,0,0,0");
+		}
 	}
 }
