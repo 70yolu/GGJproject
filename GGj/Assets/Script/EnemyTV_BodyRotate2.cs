967fdacf3b0b7000185180d8ea78b6f27af43d4d
someyoung <shosomeya@unity3d.com>
2015-01-24 13:50:28 +0900

    敵オブジェクトの回転方法を仕上げ

--- GGj/Assets/Script/EnemyTV_BodyRotate2.cs
+++ GGj/Assets/Script/EnemyTV_BodyRotate2.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class EnemyTV_BodyRotate2 : MonoBehaviour {
 	
+	float rotationStart = 0;
+
+
 	// Use this for initialization
 	void Start () {
+				
+		rotationStart = transform.localEulerAngles.y;
+
 		Up ();
 		
 		
@@ -15,14 +21,20 @@ public class EnemyTV_BodyRotate2 : MonoBehaviour {
 	}
 	
 	void Up () {
-		TweenRY.Add(transform.gameObject, 1.0f, 7.0f).Delay(0.1f).Then(Down);
+		
+		TweenRY.Add(transform.gameObject, 1.0f, rotationStart+9.0f).Delay(0.1f).Then(Down);
+
+//		TweenRY.Add(transform.gameObject, 1.0f, 7.0f).Delay(0.1f).Then(Down);
 
 //		TweenRY.Add (g:,do,float TweenRY) - CFX2_AutoRotate TweenY to value TweenRY.
 
 	}
 	
 	void Down () {
-		TweenRY.Add(transform.gameObject, 1.0f, 0.0f).Delay(0.1f).Then(Up);
+		
+		TweenRY.Add(transform.gameObject, 1.0f, rotationStart+0.0f).Delay(0.1f).Then(Up);
+
+//		TweenRY.Add(transform.gameObject, 1.0f, 0.0f).Delay(0.1f).Then(Up);
 	}
 	
 	
--- GGj/Assets/Script/EnemyTV_BodyRotate2_Slow.cs
+++ GGj/Assets/Script/EnemyTV_BodyRotate2_Slow.cs
@@ -2,9 +2,15 @@
 using System.Collections;
 
 public class EnemyTV_BodyRotate2_Slow : MonoBehaviour {
-	
+
+	float rotationStart = 0;
+
+
 	// Use this for initialization
 	void Start () {
+		
+		rotationStart = transform.localEulerAngles.y;
+
 		Up ();
 		
 		
@@ -15,14 +21,17 @@ public class EnemyTV_BodyRotate2_Slow : MonoBehaviour {
 	}
 	
 	void Up () {
-		TweenRY.Add(transform.gameObject, 2.0f, 7.0f).Delay(1.2f).Then(Down);
+
+		TweenRY.Add(transform.gameObject, 2.0f, rotationStart+7.0f).Delay(1.2f).Then(Down);
+
+//		TweenRY.Add(transform.gameObject, 2.0f, 7.0f).Delay(1.2f).Then(Down);
 		
 		//		TweenRY.Add (g:,do,float TweenRY) - CFX2_AutoRotate TweenY to value TweenRY.
 		
 	}
 	
 	void Down () {
-		TweenRY.Add(transform.gameObject, 2.0f, 0.0f).Delay(1.2f).Then(Up);
+		TweenRY.Add(transform.gameObject, 2.0f, rotationStart+0.0f).Delay(1.2f).Then(Up);
 	}
 	
 	
