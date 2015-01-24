4269fa0f1c18584c110b4b5c6316b603b7b5454e
hayahsi <70yolu@gmail.com>
2015-01-24 17:59:00 +0900

    渡しコミット

---
+++ GGj/Assets/Script/GameClearandOver/GameOverandClear.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverandClear : MonoBehaviour {
+
+	public void OnRetryBtn(){
+		Application.LoadLevel ("GameMain");
+	}
+	public void OnTitleBtn(){
+		Application.LoadLevel ("Title");
+	}
+}
