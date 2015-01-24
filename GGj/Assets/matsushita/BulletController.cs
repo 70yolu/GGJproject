ce0ed4207dcf28c646b1fba870c5525601a38b24
Hiroyuki Matsushita <corey125j@gmail.com>
2015-01-24 16:19:44 +0900

    おかしいけどとりあえずプレイヤー

---
+++ GGj/Assets/matsushita/BulletController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	public float m_Speed = 10.0f;
+	public float m_LifeTime = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+//		rigidbody.AddForce (Vector3.forward * m_Speed);
+		Destroy (gameObject, m_LifeTime);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	
+	//------------------------------------------------------------
+	// ヒット検出
+	void OnCollisionEnter(Collision	collisionInfo)
+	{
+		if (collisionInfo.gameObject.tag == "EnemyTV") 
+		{
+			collisionInfo.gameObject.SendMessage("ApplyDamage");
+		}
+//		Destroy (gameObject);
+	}
+}
---
+++ GGj/Assets/matsushita/PlayerCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public float m_Dist = 5.0f;				// ターゲットからの距離
+	public Vector3 m_TarOfs = Vector3.zero;	// ターゲットのオフセット
+	public Vector3 m_PosOfs = Vector3.zero;	// カメラ位置のオフセット
+	public float m_Smooth = 1.0f;			// カメラのスムース
+
+	public GameObject m_Target;					// ターゲット
+	public Vector3 m_TarToCamVec;				// ターゲットからカメラへのベクトル
+	private Vector3 m_TarPos;					// 現在ターゲットにしている位置
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+//		Vector3 playerBack = transform.back;
+/*
+		Vector3 camPos = transform.position + transform.forward * m_TarToCamVec;
+		Vector3 tarPos = transform.position;
+
+		Camera.main.transform.position = camPos;
+		Camera.main.transform.LookAt(tarPos);*/
+	}
+}
