3f934a129d523df6f2220697396f9f755b3b95e0
someyoung <shosomeya@unity3d.com>
2015-01-24 10:05:50 +0900

    箱爆発のスクリプトと環境音とTVの声のサウンドデータを追加

--- GGj/Assets/Script/EnemyTV_Destroy.js
+++ GGj/Assets/Script/EnemyTV_Destroy.js
@@ -1,9 +1,33 @@
 ﻿#pragma strict
 
+var explosionPrefab : GameObject; //爆発エフェクト
+
+private var damaged : boolean; //ダメージを受けた判定
+private var killTimer : float; //消滅までの時間
+
 function Start () {
+}
+
+function ApplyDamage(){
+	if(!damaged){
+		damaged = true;
+		killTimer = 0.4;
+		}
 
 }
 
 function Update () {
-
+	if(!damaged){
+		damaged = true;
+		killTimer -= Time.deltaTime;
+		if(killTimer <= 0.0) {
+			//エフェクトを出しつつ、自身のゲームオブジェクトを破壊
+			Instantiate(explosionPrefab, transform.position, transform.rotation);
+			Destroy(GameObject);
+		}
+	}
 }
+
+
+	
+
